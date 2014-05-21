e0daa026d5b9681ffc6982fd022c802122e4e578
toru-takahashi <torutakahashi.ayashi@gmail.com>
2014-05-21 15:05:00 +0900

    investigate which was error

--- main.rb
+++ main.rb
@@ -30,7 +30,9 @@ begin
   cases = desk.case_search(last_measurement_time)
   raise "cases blank" if cases.blank?
   if cases.total_entries > 0 then
+    p "cases total_entries: #{cases.total_entries}"
     for i in 0..cases.total_entries-1 do
+      p "cases: #{i}"
       deskcase = desk.case(cases.entries[i]['id'])
 
       raise "deskcase blank" if deskcase.blank?
@@ -44,7 +46,9 @@ begin
       raise "reply_list blank" if reply_list.blank?
       
       if reply_list.total_entries > 0 then
+        p "repliy total: #{reply_list.total_entries}"
         for j in 0..reply_list.total_entries-1 do
+          p "reply: #{j}"
           raise "reply_list entries blank" if reply_list.entries[j].blank?
           TD.event.post('deskreply', reply_list.entries[j].tojson) if reply_list.entries[j].created_at > last_measurement_time
           sleep(1)
